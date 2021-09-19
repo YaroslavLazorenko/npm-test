e9c938fb8d30ca1cd0395d90c7e4e3732741b128
YaroslavLazorenko <lazorenko85@gmail.com>
2021-09-20 02:51:19 +0300

    Update

---
+++ greeter.js
@@ -0,0 +1,11 @@
+// В файле greeter.js
+const helloMessage = 'hello!';
+const goodbyeMessage = 'goodbye!';
+
+const hello = () => helloMessage;
+const goodbye = () => goodbyeMessage;
+
+module.exports = {
+  hello,
+  goodbye,
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("NodeJS is amazing!");
+
+const greeter = require('./greeter');
+
+console.log(greeter.hello()); // 'hello!'
+console.log(greeter.goodbye()); // 'goodbye!'
